6fd6b04a511d026c3a6c45293b5da6d79745b4d9
Gabriel Acosta <acostagabriel126@gmail.com>
2025-05-23 19:53:51 -0300

    first commit

---
+++ src/test/java/com/admin/bank/bank_services/BankServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.bank.bank_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
